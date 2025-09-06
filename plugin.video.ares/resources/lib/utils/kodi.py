9dabf1107a79e70f8a905b5ce0bcb89561349a18
Ipnootic <ipnootic@live.com.pt>
2025-09-07 00:41:23 +0100

    .

--- plugin.video.ares/resources/lib/utils/kodi.py
+++
@@ -1,5 +0,0 @@
-# Thin wrappers to avoid Kodi API import errors when linting
-try:
-    import xbmc, xbmcgui, xbmcaddon, xbmcplugin
-except Exception:
-    xbmc = xbmcgui = xbmcaddon = xbmcplugin = None
--- plugin.video.ares/resources/lib/utils/settings.py
+++
@@ -1,7 +0,0 @@
-import xbmcaddon
-ADDON = xbmcaddon.Addon()
-def get_str(id, default=""): return ADDON.getSettingString(id) or default
-def get_bool(id): return ADDON.getSettingBool(id)
-def get_int(id, default=0): 
-    try: return int(ADDON.getSettingInt(id))
-    except: return default
